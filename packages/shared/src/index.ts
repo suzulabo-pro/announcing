1d805659d6fa49eaccea0e174478db1ec3758c65
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2021-08-04 17:55:09 +0900

    fix: web is deleted

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,6 +2,3 @@ export * from './appenv';
 export * from './datatypes';
 export * from './lazy-promise';
 export * from './path-matcher';
-export * from './web/firestore';
-export * from './web/promisestate';
-export * from './web/route';
