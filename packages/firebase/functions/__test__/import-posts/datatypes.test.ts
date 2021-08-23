54d05a5911d9ffa2373c9233f0d87e6bcfb7bc6d
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2021-08-23 16:37:44 +0900

    update test

--- packages/firebase/functions/__test__/import-posts/datatypes.test.ts
+++ packages/firebase/functions/__test__/import-posts/datatypes.test.ts
@@ -1,6 +1,6 @@
 import { validatePostsImportJSON } from '../../src/import-posts/datatypes';
 
-describe('import posts validate', () => {
+describe('validatePostsImportJSON', () => {
   const tests: [string, any, boolean][] = [
     ['empty', {}, false],
     ['ok', { posts: [{ body: 'testing', uT: '2021-09-09T12:24:56' }] }, true],
@@ -9,6 +9,24 @@ describe('import posts validate', () => {
       {
         posts: [{ body: 'testing', uT: '2021-09-09T12:24:56' }],
         lol: true,
+      },
+      false,
+    ],
+    ['no title body', { posts: [{ uT: '2021-09-09T12:24:56' }] }, false],
+    ['null title body', { posts: [{ title: null, body: null, uT: '2021-09-09T12:24:56' }] }, false],
+    ['title only', { posts: [{ title: 'title only', uT: '2021-09-09T12:24:56' }] }, true],
+    [
+      'title and body',
+      { posts: [{ title: 'title', body: 'body', uT: '2021-09-09T12:24:56' }] },
+      true,
+    ],
+    ['error time', { posts: [{ body: 'testing', uT: '2021-13-09T12:24:56' }] }, false],
+    [
+      'error img',
+      {
+        posts: [
+          { body: 'testing', img: 'http://announcing.test/test.jpg', uT: '2021-12-09T12:24:56' },
+        ],
       },
       false,
     ],
