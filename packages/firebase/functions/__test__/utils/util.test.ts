954a6a73e5de0d034ab6ebf652884874f03b0b6f
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2021-08-27 18:25:54 +0900

    add base62 of nacl key

--- packages/firebase/functions/__test__/utils/util.test.ts
+++ packages/firebase/functions/__test__/utils/util.test.ts
@@ -22,4 +22,19 @@ describe('util', () => {
       expect(s.length).toEqual(22);
     }
   });
+
+  it.skip('[EXPERIMENT] length of base62 of nacl key', () => {
+    const data00 = new Uint8Array(32).fill(0);
+    {
+      const s = bs62.encode(data00);
+      console.log(s, s.length);
+      expect(s.length).toEqual(32);
+    }
+    const dataff = new Uint8Array(32).fill(0xff);
+    {
+      const s = bs62.encode(dataff);
+      console.log(s, s.length);
+      expect(s.length).toEqual(43);
+    }
+  });
 });
