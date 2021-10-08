414d73510d5452fd7b9b86bbd6f491f2777568f8
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2021-10-08 16:03:41 +0900

    setup scripts

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,8 +1,33 @@
 import { sh } from './sh';
 
+const commands: [string, string][] = [
+  ['lint', 'eslint --ext .ts,.tsx src'],
+  ['ts-check', 'tsc --noEmit'],
+  ['shared-web.start', 'stencil build --dev --watch --config scripts/shared-web/stencil.config.ts'],
+  [
+    'console.start',
+    'stencil build --dev --watch --serve --config scripts/console/stencil.config.ts',
+  ],
+  ['console.build', 'stencil build --config scripts/console/stencil.config.ts'],
+];
+
+const commandsMap = new Map(commands);
+
 const main = async () => {
-  await sh('echo hello');
-  console.log(process.argv);
+  const cmd = process.argv[2];
+  if (!cmd) {
+    throw 'no command';
+  }
+
+  const script = commandsMap.get(cmd);
+  if (!script) {
+    throw 'invalid command';
+  }
+
+  console.info(`> ${cmd}`);
+  console.info(`> ${script}`);
+  console.info();
+  await sh(script);
 };
 
 main().catch(err => {
