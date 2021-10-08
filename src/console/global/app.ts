72ab9dcf9a40d308db2360b7e0d7b7e15972e55c
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2021-10-08 15:01:17 +0900

    build shared-web components

--- src/console/global/app.ts
+++ src/console/global/app.ts
@@ -1,20 +1,4 @@
-import '../../shared-web/components/ap-announce/ap-announce';
-import '../../shared-web/components/ap-checkbox/ap-checkbox';
-import '../../shared-web/components/ap-error/ap-error';
-import '../../shared-web/components/ap-head/ap-head';
-import '../../shared-web/components/ap-icon/ap-icon';
-import '../../shared-web/components/ap-image-input/ap-image-input';
-import '../../shared-web/components/ap-image/ap-image';
-import '../../shared-web/components/ap-input/ap-input';
-import '../../shared-web/components/ap-loading/ap-loading';
-import '../../shared-web/components/ap-modal/ap-modal';
-import '../../shared-web/components/ap-navi/ap-navi';
-import '../../shared-web/components/ap-post/ap-post';
-import '../../shared-web/components/ap-posts/ap-posts';
-import '../../shared-web/components/ap-root/ap-root';
-import '../../shared-web/components/ap-spinner/ap-spinner';
-import '../../shared-web/components/ap-style/ap-style';
-import '../../shared-web/components/ap-textview/ap-textview';
+import '../../shared-web/components';
 
 export default () => {
   return;
