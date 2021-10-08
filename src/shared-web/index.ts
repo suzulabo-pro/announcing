95eca962264ba2a86951c74f4daadc124f1ad6ab
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2021-10-08 14:44:34 +0900

    shared components

--- src/shared-web/index.ts
+++ src/shared-web/index.ts
@@ -1,15 +1,5 @@
-import { Match } from '../shared/path-matcher';
-
+export * from './datatypes';
 export * from './firestore';
 export * from './pagevisible';
 export * from './promise-state';
 export * from './route';
-
-export interface ApNaviLink {
-  label: string;
-  href?: string;
-  handler?: () => void;
-  back?: boolean;
-}
-
-export type RouteMatch = Match & { tag?: string };
