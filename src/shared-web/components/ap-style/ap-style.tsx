012f0e0342575fe004f3a0d0c7f68edd524af90b
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2021-11-06 03:26:08 +0900

    fix: body height 100vh

--- src/shared-web/components/ap-style/ap-style.tsx
+++ src/shared-web/components/ap-style/ap-style.tsx
@@ -1,10 +1,30 @@
-import { Component, h, Host } from '@stencil/core';
+import { Component, h, Host, Listen } from '@stencil/core';
+
+const handleResize = () => {
+  const wh = window.innerHeight;
+  document.documentElement.style.setProperty('--wh', `${wh}px`);
+  console.log('wh', wh);
+};
 
 @Component({
   tag: 'ap-style',
   styleUrl: 'ap-style.scss',
 })
 export class ApStyle {
+  @Listen('resize', { target: 'window' })
+  listenResize() {
+    requestAnimationFrame(handleResize);
+  }
+
+  @Listen('scroll', { target: 'window' })
+  listenScroll() {
+    requestAnimationFrame(handleResize);
+  }
+
+  componentWillLoad() {
+    requestAnimationFrame(handleResize);
+  }
+
   render() {
     return <Host></Host>;
   }
