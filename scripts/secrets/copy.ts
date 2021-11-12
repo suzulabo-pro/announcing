e73a516cd453d7bee9268c0aeac58ad93be0d314
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2021-11-12 14:44:38 +0900

    fix

--- scripts/secrets/copy.ts
+++ scripts/secrets/copy.ts
@@ -8,6 +8,11 @@ export const copySecrets = () => {
       continue;
     }
     console.info(`${sec.name} -> ${sec.location}/${sec.name}`);
+
+    if (!fs.existsSync(sec.location)) {
+      fs.mkdirSync(sec.location);
+    }
+
     fs.copyFileSync(path.join(SECRET_DIR, sec.name), path.join(ROOT_DIR, sec.location, sec.name));
   }
 };
